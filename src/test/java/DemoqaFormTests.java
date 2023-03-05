48a54d2d51da5f831944651d0a0e926896154481
Kurbatika <73817065+Kurbatika@users.noreply.github.com>
2023-03-05 19:17:22 +0300

    fixed mistakes

--- src/test/java/DemoqaFormTests.java
+++ src/test/java/DemoqaFormTests.java
@@ -1,25 +1,30 @@
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
-
-import javax.swing.*;
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class DemoqaFormTests {
-    @Test
-    void formFulfillmentTest () {
+   @BeforeAll
+        static void beforeAll() {
         Configuration.browserSize = "3020x2048";
+    }
+
+   @Test
+    void formFulfillmentTest () {
 
         open("https://demoqa.com/automation-practice-form");
 
-        executeJavaScript("$('footer').remove()"); executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
 
         $("[placeholder='First Name']").setValue("Michael");
+
 
         $("[placeholder='Last Name']").setValue("Jackson");
 
@@ -27,28 +32,42 @@ public class DemoqaFormTests {
 
         $("#gender-radio-1").sendKeys(" ");
 
-        $("#userNumber").setValue("+1234567890");
+        $("#userNumber").setValue("1234567890");
 
-        $("#dateOfBirthInput").click();
-        $("#dateOfBirthInput").setValue("29 Aug 1958");
-        $("#dateOfBirthInput").pressEnter();
+       $("#dateOfBirthInput").click();
+       $(".react-datepicker__month-select").selectOption("August");
+       $(".react-datepicker__year-select").selectOption("1958");
+       $(".react-datepicker__day--029:not(.react-datepicker__day--outside-month)").click();
 
-        $("#subjectsInput").setValue("Computer science").pressEnter();
 
-        $("#hobbies-checkbox-3").sendKeys(" ");
+       $("#subjectsInput").setValue("Computer science").pressEnter();
 
-        $("#uploadPicture").uploadFile(new File("C:/Users/kurba/IdeaProjects/qaguru_hw3/src/test/resources/pictures/michael.jpg"));
+       $("#hobbies-checkbox-3").sendKeys(" ");
 
-        $("#currentAddress").setValue("Los-Angeles, CA");
+       $("#uploadPicture").uploadFile(new File("C:/Users/kurba/IdeaProjects/qaguru_hw3/src/test/resources/pictures/michael.jpg"));
 
-        executeJavaScript("window.scrollTo(0, document.body.scrollHeight)");
-        $(byText("Select State")).click();
-        $(byText("NCR")).click();
+       $("#currentAddress").setValue("Los-Angeles, CA");
 
-        $(byText("Select City")).click();
-        $(byText("Delhi")).click();
+       executeJavaScript("window.scrollTo(0, document.body.scrollHeight)");
+       $(byText("Select State")).click();
+       $(byText("NCR")).click();
 
-        $("#submit").click();
+       $(byText("Select City")).click();
+       $(byText("Delhi")).click();
+
+       $("#submit").click();
+
+       $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+       $("tbody tr:nth-child(1) td:nth-child(2)").shouldHave(text("Michael Jackson"));
+       $("tbody tr:nth-child(2) td:nth-child(2)").shouldHave(text("michael@jackson.com"));
+       $("tbody tr:nth-child(3) td:nth-child(2)").shouldHave(text("Male"));
+       $("tbody tr:nth-child(4) td:nth-child(2)").shouldHave(text("1234567890"));
+       $("tbody tr:nth-child(5) td:nth-child(2)").shouldHave(text("29 August,1958"));
+       $("tbody tr:nth-child(7) td:nth-child(2)").shouldHave(text("Music"));
+       $("tbody tr:nth-child(8) td:nth-child(2)").shouldHave(text("michael.jpg"));
+       $("tbody tr:nth-child(9) td:nth-child(2)").shouldHave(text("Los-Angeles, CA"));
+       $("tbody tr:nth-child(10) td:nth-child(2)").shouldHave(text("NCR Delhi"));
+       $("#closeLargeModal").exists();
 
     }
 }
